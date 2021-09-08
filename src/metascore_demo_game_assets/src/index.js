f8042235a71b5632ed0504051c6e20f722f90295
Tarek Mostafa <tarek.mostafa@zalando.de>
2021-09-08 06:38:49 +0200

    initial commit

--- src/metascore_demo_game_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { metascore_demo_game } from "../../declarations/metascore_demo_game";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with metascore_demo_game actor, calling the greet method
-  const greeting = await metascore_demo_game.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
